3ac28fd64ea408309323d79a42cac757f3114590
dvcone <dani2014riau@gmail.com>
2022-06-03 20:55:45 +0700

    030622 e

--- test/dicoding_test/06_main.dart
+++ test/dicoding_test/06_main.dart
@@ -1,8 +1,16 @@
 import '06_animal.dart';
+import '06_extension.dart';
  
 void main() {
+  // Properties & Method Module
   var garfieldCat = Animal('Garfield', 2, 4.2);
   garfieldCat.eat();
   garfieldCat.poop();
   print(garfieldCat.weight);
+
+  // Extension Module
+  var unsortedNumbers = [2, 5, 3, 1, 4];
+  print(unsortedNumbers);
+  var sortedNumbers = unsortedNumbers.sortAsc();
+  print(sortedNumbers);
 }
